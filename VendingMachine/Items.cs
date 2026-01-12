bf4f3093c278769b6d2e46bdf218162e0f3519e2
Waylate1 <Weighall44@gmail.com>
2026-01-12 13:03:14 -0800

    Add project files.

---
+++ VendingMachine/Items.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    public class Items
+    {
+        int UserChoice = 0;
+        public void Run()
+        {
+            DisplayItems();
+            Dispense();
+
+        }
+        public void DisplayItems()
+        {
+            Console.WriteLine("1. Water");
+            Console.WriteLine("2. Coca Cola");
+            Console.WriteLine("3. Red Bull");
+        }
+
+        public void Dispense()
+        {
+            UserChoice = GetSelection(UserChoice);
+            if (UserChoice == 1)
+            {
+                Console.WriteLine("Dispensing water");
+            } 
+            if(UserChoice == 2)
+            {
+                Console.WriteLine("Dispensing Coca Cola");
+            }
+            if(UserChoice == 3)
+            {
+                Console.WriteLine("Dispensing Red Bull");
+            }
+        }
+
+        public int GetSelection(int i)
+        {
+            int UserChoice = 0;
+            Console.WriteLine("Enter a displayed number for the item");
+            while (!int.TryParse(Console.ReadLine(), out UserChoice) || !ValidateChoice(UserChoice))
+            {
+                Console.WriteLine("Please enter a valid choice");
+            }
+            return UserChoice;
+        }
+
+        private bool ValidateChoice(int UserChoice)
+        {
+            return UserChoice >= 1 && UserChoice <= 3;
+        }
+    }
+}
---
+++ VendingMachine/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace VendingMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Items myitems = new Items();
+            myitems.Run();
+            
+
+        }
+    }
+}
